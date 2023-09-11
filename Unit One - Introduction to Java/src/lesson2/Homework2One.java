8d5238678bfaf31255ee315f6bf7376689ca0574
JolineLian <jlian2@bayviewglen.ca>
2023-09-11 12:34:09 -0400

    day 2 homework

---
+++ Unit One - Introduction to Java/src/lesson2/Homework2One.java
@@ -0,0 +1,12 @@
+package lesson2;
+
+public class Homework2One {
+    public static void main(String[] args) {
+        System.out.println("Question: What are two things you can never eat for breakfast?");
+        System.out.println("A) Pizza");
+        System.out.println("B) Lunch");
+        System.out.println("C) Dinner");
+        System.out.println("D) Both B and C");
+        System.out.print("Correct Answer: D) Both B and C");
+    }
+}
---
+++ Unit One - Introduction to Java/src/lesson2/Homework2Two.java
@@ -0,0 +1,14 @@
+package lesson2;
+
+public class Homework2Two {
+    public static void main(String[] args) {
+       System.out.println("Presentation: Exploring Huron-Wendat Heritage");
+       System.out.println("Aspecs of Huron-Wendat Culture:"); 
+       System.out.println("- Traditional Practices");
+       System.err.println("- Art and Craftmanship");
+       System.out.println("- language and Storytelling");
+       System.out.println("Community Traditions");
+       System.err.println("Date: Wednesdy, November 15, 2023");
+       System.out.println("Duration: 60 minutes");
+    }
+}
