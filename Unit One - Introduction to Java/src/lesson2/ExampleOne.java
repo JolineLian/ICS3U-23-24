dd2f79b06f784c1399c83f2afb7a47ab5810985a
JolineLian <jlian2@bayviewglen.ca>
2023-09-11 11:15:44 -0400

    initial commit

--- Unit One - Introduction to Java/src/lesson2/ExampleOne.java
+++ Unit One - Introduction to Java/src/lesson2/ExampleOne.java
@@ -3,5 +3,6 @@ package lesson2;
 public class ExampleOne {
     public static void main(String[] args) {
         System.out.println("hello ICS3U!");
+        System.out.println("goodbye ICS3U!");
     }
 }
