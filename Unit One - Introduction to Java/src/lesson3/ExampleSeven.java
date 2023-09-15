25ef1a97c38638f954c96e8741d1af9b5bddf95d
JolineLian <jlian2@bayviewglen.ca>
2023-09-15 11:16:18 -0400

    Lesson 3 Examples

---
+++ Unit One - Introduction to Java/src/lesson3/ExampleSeven.java
@@ -0,0 +1,19 @@
+package lesson3;
+
+public class ExampleSeven {
+    public static void main(String[] args) {
+        int numberOfStudents = 15;
+
+        numberOfStudents += 3;
+
+        System.out.println(numberOfStudents);
+
+        //compount assignment operators +=, -=, %=, /=, *=
+
+        numberOfStudents++; // adds 1 and reassigns to numberOfStudents
+
+        numberOfStudents--; // does the same as above but subtraction
+
+        
+    }
+}
