aa23add6eb5b8c0bd0272537765f4f444c6f4ee8
JolineLian <jlian2@bayviewglen.ca>
2023-09-25 14:18:14 -0400

    HomeworkQ3 Edit

--- Unit One - Introduction to Java/src/lesson3/HomeworkQ3.java
+++ Unit One - Introduction to Java/src/lesson3/HomeworkQ3.java
@@ -2,8 +2,8 @@ package lesson3;
 
 public class HomeworkQ3 {
     public static void main(String[] args) {
-        int examOne, examTwo, examThree, examFour, examFive; //needs to be initialized
-
+        int examOne = 0, examTwo = 0, examThree = 0, examFour = 0, examFive = 0;
+        
         double avgScore = (examOne + examTwo + examThree + examFour + examFive) / 5;
         System.out.println(avgScore);
     }
